be1e957f438fc3351d3f139730d67e91da670f53
Hichou <105111612+leohichou@users.noreply.github.com>
2024-05-07 23:59:46 +0100

    Update Exercice3.js

--- Atelier 3 Javascript et Functional Programming/Exercice3.js
+++ Atelier 3 Javascript et Functional Programming/Exercice3.js
@@ -1,16 +1,23 @@
 // Exercice 3 : Ecrire un programme fonctionnel qui divise le texte en lignes, convertit chaque ligne en majuscules,
 //filtre les lignes contenant la lettre "I", puis les imprime.
+function filtrerLignesContenantI(texte) {
+  // Diviser le texte en lignes
+  const lignes = texte.split('\n');
+  
+  // Convertir chaque ligne en majuscules, filtrer les lignes contenant la lettre "I"
+  const lignesFiltrees = lignes
+    .map(ligne => ligne.toUpperCase())
+    .filter(ligne => ligne.includes('I'));
 
-function convertMaj(text){
-    return uppercase = text.toUpperCase();
-
+  // Imprimer les lignes filtrées
+  lignesFiltrees.forEach(ligne => console.log(ligne));
 }
 
-function filterI(text){
-  return   filteredLetters = text.split('').filter(char => char === 'I');
-}
-
-a= convertMaj('jekfhkjhfjkfbffebfioib kefwhknfskjhfwhjnfi feknjkfbf i');
-console.log(a);
-b=filterI(a);
-console.log (b);
+// Exemple d'utilisation
+const texte = `hello
+Hi
+My 
+name
+is 
+`;
+filtrerLignesContenantI(texte);
